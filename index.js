293e040ecce64e2520a6beb5354d0462c16e4878
Grégory Viguier <greg@disroot.org>
2019-05-29 17:37:41 +0200

    Add Winston Logging

--- index.js
+++ index.js
@@ -5,13 +5,28 @@ const { promisify } = require('util');
 // Express
 const app = express();
 const port = 7001;
+app.use(express.json()); // for parsing application/json
 
 // LDAP
 const rootDN = 'OU=Users,DC=exp,DC=synchrotron-soleil,DC=fr';
 const baseDN = 'CN=service_jra2,OU=services,OU=Users,DC=exp,DC=synchrotron-soleil,DC=fr';
 const jra2LdapSecret = 'S3rv1c3JR42#?';
 
-app.use(express.json()); // for parsing application/json
+// Winston
+const { format, createLogger, transports } = require('winston');
+
+const logger = createLogger({
+  format: format.combine(
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
+    }),
+    format.simple(),
+  ),
+  transports: [
+    new transports.File({ filename: 'error.log', level: 'error' }),
+    new transports.File({ filename: 'combined.log' }),
+  ],
+});
 
 function error(status, msg) {
   const err = new Error(msg);
@@ -24,13 +39,14 @@ const validateInput = function validateUsernameAndPassword(req, res, next) {
   const { password } = req.body;
 
   if (username === undefined || password === undefined) {
+    logger.error("Expected 'username' and 'password'");
     next(error(400, "Expected 'username' and 'password'"));
   } else {
     next();
   }
 };
 
-app.post('/login', validateInput, (req, res, next) => {
+app.post('/login', validateInput, async (req, res, next) => {
   const { username } = req.body;
   const { password } = req.body;
 
@@ -41,28 +57,33 @@ app.post('/login', validateInput, (req, res, next) => {
     url: 'ldap://localhost:1389',
   });
 
-  client.bind(baseDN, jra2LdapSecret, (err) => {
-    if (err) {
-      next(error(400, 'Cannot bind LDAP'));
-    }
-    const opts = {
-      filter: `cn=${username}`,
-      scope: 'sub',
-      attributes: ['dn'],
-    };
+  const bindAsync = promisify(client.bind).bind(client);
+  try {
+    await bindAsync(baseDN, jra2LdapSecret);
+  } catch (err) {
+    logger.error(err);
+    return next(error(400, 'Cannot bind LDAP'));
+  }
 
-    client.search(rootDN, opts, (_, searchRes) => {
-      searchRes.once('searchEntry', (entry) => {
-        const { dn } = entry.object;
-        client.bind(dn, password, (bindErr) => {
-          if (bindErr) {
-            next(error(401, 'Unauthorized'));
-          }
+  const opts = {
+    filter: `cn=${username}`,
+    scope: 'sub',
+    attributes: ['dn'],
+  };
+
+  client.search(rootDN, opts, (_, searchRes) => {
+    searchRes.once('searchEntry', (entry) => {
+      const { dn } = entry.object;
+      client.bind(dn, password, (bindErr) => {
+        if (bindErr) {
+          logger.error(`User ${dn} not authorized (incorrect password)`);
+          next(error(401, 'Unauthorized'));
+        } else {
           res.status(status).send(message);
-        });
+        }
       });
-      searchRes.on('end', () => client.unbind());
     });
+    searchRes.on('end', () => client.unbind());
   });
 });
 
